5c1017de42c33fd0f07756c0142d8c7b617b1ae2
VeselinovStf <chofexx@gmail.com>
2018-03-29 13:23:40 +0300

    Adding new project

---
+++ 09_CreatingAndUsingObjectsExercise/CSharpIntroBook/SchoolInfo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpIntroBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
