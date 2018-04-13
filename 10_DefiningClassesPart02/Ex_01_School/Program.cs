78e33c19ad91122582746a36537d6f4c365e9ce7
VeselinovStf <chofexx@gmail.com>
2018-04-13 07:53:01 +0300

    Adding 10_DefiningClassesPart02.sln

---
+++ 10_DefiningClassesPart02/Ex_01_School/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex_01_School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
