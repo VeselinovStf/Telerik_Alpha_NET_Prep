3874e2b52e705bcfed1a4cc1b1138ccc7721ca16
VeselinovStf <chofexx@gmail.com>
2018-04-16 17:26:51 +0000

    Add first exe
    first exercise done

---
+++ 11_ExtensionMethods_DelegatesLinq/01_StringBuilderSubstring/MyExtensionsMethods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace _01_StringBuilderSubstring
+{
+    public static class MyExtensionsMethods
+    {
+        public static StringBuilder Substring(this StringBuilder str, int index, int length)
+        {
+            StringBuilder result = new StringBuilder();
+
+            int newStringLength = index + length;
+            if (index < 0 || length < 0)
+            {
+                throw new ArgumentException("Index and length cant be negative");
+            }
+
+            if (newStringLength > str.Length)
+            {
+                throw new ArgumentException("Start String + Length are bigger than a string length");
+            }
+            else
+            {
+                for (int i = index; i < newStringLength; i++)
+                {
+                    result.Append(str[i] + "-");
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 11_ExtensionMethods_DelegatesLinq/01_StringBuilderSubstring/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace _01_StringBuilderSubstring
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string testLine = "123456789";
+            StringBuilder text = new StringBuilder();
+            text.Append(testLine);
+            
+            
+            Console.WriteLine(testLine.Substring(2,4));
+            Console.WriteLine(text.Substring(2, 4).ToString());
+        }
+    }
+}
