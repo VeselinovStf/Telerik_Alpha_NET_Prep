24ffcb099a0575816652db2a40c3f674aa1f08e2
VeselinovStf <chofexx@gmail.com>
2018-05-05 17:00:23 +0300

    Separating Data logic from Model, Adding Migrations

---
+++ Web_Applications/ASP_NET/MVC/LectureExercise/LectureExercise.Forum.Data/MsSqlDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LectureExercise.Forum.Data.Model;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace LectureExercise.Forum.Data
+{
+
+    public class MsSqlDbContext : IdentityDbContext<User>
+    {
+        public MsSqlDbContext()
+            : base("LocalConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static MsSqlDbContext Create()
+        {
+            return new MsSqlDbContext();
+        }
+    }
+}
