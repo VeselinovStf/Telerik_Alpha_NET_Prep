7d101671e05f681d802b1512920839757060c6f6
VeselinovStf <chofexx@gmail.com>
2018-05-14 12:30:44 +0300

    Exercises done

---
+++ AlphaExamPrep/TAA_EntranceExamOctober13/01_Yesterday/StartUp.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * By a provided date (year, month, day), calculate the day before.
+ * 
+ */
+
+using System;
+using System.Globalization;
+
+namespace _01_Yesterday
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+            string inputYear = Console.ReadLine();
+            string inputMonth = Console.ReadLine();
+            string inputDay = Console.ReadLine();
+
+            string s = inputDay + "-" + inputMonth + "-" + inputYear;
+            string format = "dd-M-yyyy";
+            var provider = CultureInfo.InvariantCulture;
+
+            DateTime currentInputDate = DateTime.ParseExact(s, format, provider);
+
+            var newDate = currentInputDate.Subtract(TimeSpan.FromDays(1));
+
+            Console.WriteLine(newDate.ToString("d-MMM-yyy"));
+        }
+    }
+}
