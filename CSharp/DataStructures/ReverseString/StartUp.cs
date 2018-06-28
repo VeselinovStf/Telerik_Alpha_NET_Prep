ebb2bed52df283d222f366bcdcc40d3fd98679a8
VeselinovStf <chofexx@gmail.com>
2018-06-28 06:38:48 +0300

    add recent exercises

---
+++ CSharp/DataStructures/ReverseString/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string inputWord = Console.ReadLine();
+
+            Stack<char> letters = new Stack<char>();
+
+            for (int i = 0; i < inputWord.Length; i++)
+            {
+                letters.Push(inputWord[i]);
+            }
+
+            string reversed = string.Empty;
+            for (int i = 0; i < inputWord.Length; i++)
+            {
+                reversed += letters.Pop();
+            }
+
+            Console.WriteLine(reversed);
+        }
+    }
+}
---
+++ CSharp/DataStructures/SimpleCalculator/StartUp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] expresion = Console.ReadLine().Split(new char[] { ' ' },StringSplitOptions.RemoveEmptyEntries);
+
+            Stack<int> values = new Stack<int>();
+
+            char lastSign = ' ';
+
+            for (int i = 0; i < expresion.Length; i++)
+            {
+                int newNumber;
+
+                bool isNumber = int.TryParse(expresion[i], out newNumber);
+                if (isNumber)
+                {
+
+                    //is digit
+                    if (values.Count == 0)
+                    {
+                        values.Push(newNumber);
+                    }
+                    else
+                    {
+                        var lastNumber = values.Pop();
+
+                        if (lastSign == '+')
+                        {
+                            var newValue = lastNumber + newNumber;
+                            values.Push(newValue);
+                        }
+                        else
+                        {
+                            var newValue = lastNumber - newNumber;
+                            values.Push(newValue);
+                        }
+                    }
+                    
+                }
+                else
+                {
+                    lastSign = char.Parse(expresion[i]);
+                }
+
+                
+            }
+
+            Console.WriteLine(values.Pop());
+        }
+    }
+}
