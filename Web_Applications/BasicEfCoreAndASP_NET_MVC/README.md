1004cab8f95d734e251e3aac1288f1c7a8adf6a6
VeselinovStf <veselinovStf@gmail.com>
2019-04-11 08:03:15 +0300

    Initial project skeleton

---
+++ Web_Applications/BasicEfCoreAndASP_NET_MVC/README.md
@@ -0,0 +1,5 @@
+# Microsoft Tutorial: Get started with EF Core in an ASP.NET MVC web app
+
+###### This tutorial teaches ASP.NET Core MVC and Entity Framework Core with controllers and views. 
+###### Razor Pages is a new alternative in ASP.NET Core 2.0, a page-based programming model that makes
+###### building web UI easier and more productive.
---
+++ Web_Applications/BasicEfCoreAndASP_NET_MVC/UniversiteySystem.Data/UniversitySystemDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UniversiteySystem.Data
+{
+    public class UniversitySystemDbContext : DbContext
+    {      
+        public UniversitySystemDbContext()
+        {
+
+        }
+        public UniversitySystemDbContext(DbContextOptions<UniversitySystemDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+    }
+}
